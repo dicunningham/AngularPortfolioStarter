204555d12c5f2b1b351e8fb82c5d2e13f977ea33
Danie Cunningham <dicunningham@unomaha.edu>
2026-06-23 19:36:31 -0500

    feat(routing): setup admin routes with guard and component structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,8 +4,18 @@ import { AboutPage } from './pages/about-page/about-page';
 import { SkillsPage } from './pages/skills-page/skills-page';
 import { ProjectsPage } from './pages/projects-page/projects-page';
 import { ContactPage } from './pages/contact-page/contact-page';
+import { authGuard } from './guards/auth-guard';
+import { Login } from './admin/login/login';
+import { Dashboard } from './admin/dashboard/dashboard';
+
 
 export const routes: Routes = [
+    { path: 'admin/login', component: Login },
+    { 
+    path: 'admin/dashboard', 
+    component: Dashboard, 
+    canActivate: [authGuard]
+  },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomePage },
     { path: 'about', component: AboutPage },
---
+++ src/app/guards/auth-guard.ts
@@ -0,0 +1,17 @@
+import { inject } from '@angular/core';
+import { Router, CanActivateFn } from '@angular/router';
+import { AuthService } from '../services/auth';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  // Check if a user is currently logged in
+  if (authService.auth.currentUser) {
+    return true;
+  } else {
+    // Redirect to login if not authenticated
+    router.navigate(['/admin/login']);
+    return false;
+  }
+};
