edaa4c204877aaafb2f43c06d5cfb767b2d383ea
Danie Cunningham <dicunningham@unomaha.edu>
2026-06-30 03:15:20 -0500

    feat(admin): build out minimalist bento dashboard layout with secure routing architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,15 +7,27 @@ import { ContactPage } from './pages/contact-page/contact-page';
 import { authGuard } from './guards/auth-guard';
 import { LoginComponent } from './admin/login/login.component';
 import { Dashboard } from './admin/dashboard/dashboard';
+import { ManageProjects } from './admin/manage-projects/manage-projects';
+import { ManageSkills } from './admin/manage-skills/manage-skills';
 
 
 export const routes: Routes = [
     { path: 'admin/login', component: LoginComponent },
     { 
-    path: 'admin/dashboard', 
-    component: Dashboard, 
-    canActivate: [authGuard]
-  },
+      path: 'admin/dashboard', 
+      component: Dashboard, 
+      canActivate: [authGuard]
+    },
+    {
+      path: 'admin/projects',
+      component: ManageProjects,
+      canActivate: [authGuard]
+    },
+    {
+      path: 'admin/skills',
+      component: ManageSkills,
+      canActivate: [authGuard]
+    },
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomePage },
     { path: 'about', component: AboutPage },
